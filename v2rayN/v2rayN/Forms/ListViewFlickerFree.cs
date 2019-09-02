1bbc73f963157e3e011fe59d1c7d579d8e118a51
2dust <qingyede@gmail.com>
2019-09-02 11:08:15 +0800

    Update ListViewFlickerFree.cs

--- v2rayN/v2rayN/Forms/ListViewFlickerFree.cs
+++ v2rayN/v2rayN/Forms/ListViewFlickerFree.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace v2rayN.Forms
 {
@@ -10,6 +11,46 @@ namespace v2rayN.Forms
                     | ControlStyles.AllPaintingInWmPaint
                     , true);
             UpdateStyles();
+        }
+
+
+        public void AutoResizeColumns()
+        {
+            try
+            {
+                int count = this.Columns.Count;
+                int MaxWidth = 0;
+                Graphics graphics = this.CreateGraphics();
+                Font font = this.Font;
+                ListView.ListViewItemCollection items = this.Items;
+
+                string str;
+                int width;
+
+                this.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+                for (int i = 0; i < count; i++)
+                {
+                    str = this.Columns[i].Text;
+                    MaxWidth = this.Columns[i].Width;
+
+                    foreach (ListViewItem item in items)
+                    {
+                        str = item.SubItems[i].Text;
+                        width = (int)graphics.MeasureString(str, font).Width;
+                        if (width > MaxWidth)
+                        {
+                            MaxWidth = width;
+                        }
+                    }
+                    if (i == 0)
+                    {
+                        this.Columns[i].Width = MaxWidth;
+                    }
+                    this.Columns[i].Width = MaxWidth;
+                }
+            }
+            catch { }
         }
     }
 }
