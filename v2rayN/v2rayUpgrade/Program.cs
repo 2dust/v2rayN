86229bf73bd57375718420ac4452e917a5cfb7ca
2dust <31833384+2dust@users.noreply.github.com>
2019-12-12 10:04:12 +0800

    add upgrade

---
+++ v2rayN/v2rayUpgrade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v2rayUpgrade
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                MessageBox.Show("Please use v2rayN to upgrade(请用v2rayN升级)");
+                return;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm(args));
+        }
+    }
+}
