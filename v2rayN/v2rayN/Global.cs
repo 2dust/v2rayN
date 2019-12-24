6a0bfc43dd61db5f63cc3ca6e83953f02cada660
2dust <31833384+2dust@users.noreply.github.com>
2019-12-24 09:01:10 +0800

    Update Global.cs

--- v2rayN/v2rayN/Global.cs
+++ v2rayN/v2rayN/Global.cs
@@ -162,7 +162,7 @@ namespace v2rayN
         }
         public const string StatisticLogOverall = "StatisticLogOverall.json";
 
-        public const string IEProxyExceptions = "<local>;localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;172.32.*";
+        public const string IEProxyExceptions = "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*";
 
         #endregion
 
