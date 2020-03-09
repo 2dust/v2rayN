daa358a0cabdc272a6ee0a88fa1f1d719a2ab136
2dust <31833384+2dust@users.noreply.github.com>
2020-03-09 11:20:32 +0800

    Update AssemblyInfo.cs

--- v2rayN/v2rayN/Properties/AssemblyInfo.cs
+++ v2rayN/v2rayN/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 //[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("3.5")]
+[assembly: AssemblyFileVersion("3.6")]
