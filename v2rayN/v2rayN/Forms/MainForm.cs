ae79d8c608c9c643ffc20031cbf9ad3a9e2ae342
2dust <31833384+2dust@users.noreply.github.com>
2021-01-23 12:44:51 +0800

    Update MainForm.cs

--- v2rayN/v2rayN/Forms/MainForm.cs
+++ v2rayN/v2rayN/Forms/MainForm.cs
@@ -1440,6 +1440,7 @@ namespace v2rayN.Forms
                         int ret = MainFormHandler.Instance.AddBatchServers(config, result, id);
                         if (ret > 0)
                         {
+                            RefreshServers();
                         }
                         else
                         {
