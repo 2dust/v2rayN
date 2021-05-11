777427787a964b1d8b63c70282d9ca7547424298
2dust <31833384+2dust@users.noreply.github.com>
2021-05-11 20:10:02 +0800

    remove PasswordChar

--- v2rayN/v2rayN/Forms/AddServer3Form.Designer.cs
+++ v2rayN/v2rayN/Forms/AddServer3Form.Designer.cs
@@ -55,14 +55,15 @@
             // 
             // btnClose
             // 
-            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             resources.ApplyResources(this.btnClose, "btnClose");
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnClose.Name = "btnClose";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.label13);
             this.groupBox1.Controls.Add(this.cmbSecurity);
             this.groupBox1.Controls.Add(this.txtRemarks);
@@ -74,7 +75,6 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.txtAddress);
             this.groupBox1.Controls.Add(this.label1);
-            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
             // 
@@ -85,6 +85,7 @@
             // 
             // cmbSecurity
             // 
+            resources.ApplyResources(this.cmbSecurity, "cmbSecurity");
             this.cmbSecurity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbSecurity.FormattingEnabled = true;
             this.cmbSecurity.Items.AddRange(new object[] {
@@ -94,7 +95,6 @@
             resources.GetString("cmbSecurity.Items3"),
             resources.GetString("cmbSecurity.Items4"),
             resources.GetString("cmbSecurity.Items5")});
-            resources.ApplyResources(this.cmbSecurity, "cmbSecurity");
             this.cmbSecurity.Name = "cmbSecurity";
             // 
             // txtRemarks
@@ -144,9 +144,9 @@
             // 
             // panel2
             // 
+            resources.ApplyResources(this.panel2, "panel2");
             this.panel2.Controls.Add(this.btnClose);
             this.panel2.Controls.Add(this.btnOK);
-            resources.ApplyResources(this.panel2, "panel2");
             this.panel2.Name = "panel2";
             // 
             // btnOK
@@ -163,22 +163,22 @@
             // 
             // menuServer
             // 
+            resources.ApplyResources(this.menuServer, "menuServer");
             this.menuServer.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.MenuItem1});
-            resources.ApplyResources(this.menuServer, "menuServer");
             this.menuServer.Name = "menuServer";
             // 
             // MenuItem1
             // 
+            resources.ApplyResources(this.MenuItem1, "MenuItem1");
             this.MenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.menuItemImportClipboard});
             this.MenuItem1.Name = "MenuItem1";
-            resources.ApplyResources(this.MenuItem1, "MenuItem1");
             // 
             // menuItemImportClipboard
             // 
-            this.menuItemImportClipboard.Name = "menuItemImportClipboard";
             resources.ApplyResources(this.menuItemImportClipboard, "menuItemImportClipboard");
+            this.menuItemImportClipboard.Name = "menuItemImportClipboard";
             this.menuItemImportClipboard.Click += new System.EventHandler(this.menuItemImportClipboard_Click);
             // 
             // AddServer3Form
