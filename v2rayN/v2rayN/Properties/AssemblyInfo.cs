e2f399eeb1bdf8585b4e95e460ffd4bed4bcb0f8
2dust <31833384+2dust@users.noreply.github.com>
2022-04-03 17:03:21 +0800

    Update AssemblyInfo.cs

--- v2rayN/v2rayN/Properties/AssemblyInfo.cs
+++ v2rayN/v2rayN/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("v2rayN")]
-[assembly: AssemblyCopyright("Copyright © 2019-2020 (GPLv3)")]
+[assembly: AssemblyCopyright("Copyright © 2019-2022 (GPLv3)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 //[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("5.9")]
+[assembly: AssemblyFileVersion("5.10")]
