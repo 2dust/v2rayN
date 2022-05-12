6f485141f08519a5d5d7f49eec5298ba000018b8
2dust <31833384+2dust@users.noreply.github.com>
2022-05-12 09:19:03 +0800

    Improve speed test core support

--- v2rayN/v2rayN/Handler/V2rayHandler.cs
+++ v2rayN/v2rayN/Handler/V2rayHandler.cs
@@ -254,7 +254,7 @@ namespace v2rayN.Handler
 
             try
             {
-                string fileName = V2rayFindexe(new List<string> { "xray" });
+                string fileName = V2rayFindexe(new List<string> { "xray", "wv2ray", "v2ray" });
                 if (fileName == "") return -1;
 
                 Process p = new Process
