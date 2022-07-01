f7e3eb180bc9354df811e936d2a1174a8da95277
2dust <31833384+2dust@users.noreply.github.com>
2022-07-01 19:57:22 +0800

    bug fix

--- v2rayN/v2rayN/Forms/MainForm.cs
+++ v2rayN/v2rayN/Forms/MainForm.cs
@@ -799,12 +799,20 @@ namespace v2rayN.Forms
         private void menuExport2ClientConfig_Click(object sender, EventArgs e)
         {
             int index = GetLvSelectedIndex();
+            if (index < 0)
+            {
+                return;
+            }
             MainFormHandler.Instance.Export2ClientConfig(lstVmess[index], config);
         }
 
         private void menuExport2ServerConfig_Click(object sender, EventArgs e)
         {
             int index = GetLvSelectedIndex();
+            if (index < 0)
+            {
+                return;
+            }
             MainFormHandler.Instance.Export2ServerConfig(lstVmess[index], config);
         }
 
@@ -1244,7 +1252,6 @@ namespace v2rayN.Forms
             int index = GetLvSelectedIndex();
             if (index < 0)
             {
-                UI.Show(ResUI.PleaseSelectServer);
                 return;
             }
             if (ConfigHandler.MoveServer(ref config, ref lstVmess, index, eMove) == 0)
