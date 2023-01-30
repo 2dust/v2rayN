7052b56069504936a3960e806bfb5a26ff89b8bd
2dust <31833384+2dust@users.noreply.github.com>
2023-01-30 09:36:27 +0800

    Optimize font

---
+++ v2rayN/v2rayN/Converters/MaterialDesignFonts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+
+namespace v2rayN.Converters
+{
+    public class MaterialDesignFonts
+    {
+        public static FontFamily MyFont { get; }
+
+        static MaterialDesignFonts()
+        {
+            var fontPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Resources\Fonts\");
+            MyFont = new FontFamily(new Uri($"file:///{fontPath}"), "./#微软雅黑");
+        }
+    }
+}
