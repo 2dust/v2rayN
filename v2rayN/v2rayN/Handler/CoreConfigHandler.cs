785a30e62333476b51c3480f02249abe4f79fbbd
2dust <31833384+2dust@users.noreply.github.com>
2023-03-26 20:05:51 +0800

    Update CoreConfigHandler.cs

--- v2rayN/v2rayN/Handler/CoreConfigHandler.cs
+++ v2rayN/v2rayN/Handler/CoreConfigHandler.cs
@@ -1390,7 +1390,7 @@ namespace v2rayN.Handler
                     Utils.SaveLog(ex.Message, ex);
                 }
 
-                log(configCopy, ref v2rayConfig, false);
+                log(configCopy, ref v2rayConfig, true);
                 //routing(config, ref v2rayConfig);
                 //dns(configCopy, ref v2rayConfig);
 
