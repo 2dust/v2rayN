a522a02ba24977881900619ede25e2dffaf696ca
2dust <31833384+2dust@users.noreply.github.com>
2024-02-29 09:39:12 +0800

    Temporarily add compatibility to VLESS

--- v2rayN/v2rayN/Handler/ConfigHandler.cs
+++ v2rayN/v2rayN/Handler/ConfigHandler.cs
@@ -920,6 +920,10 @@ namespace v2rayN.Handler
             {
                 return -1;
             }
+            if (!Utile.IsNullOrEmpty(profileItem.security) && profileItem.security != "none")
+            {
+                profileItem.security = "none";
+            }
 
             AddServerCommon(config, profileItem, toFile);
 
