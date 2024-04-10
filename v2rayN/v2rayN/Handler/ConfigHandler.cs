d961ea22a6fe6777afbdb6e0ff807b37e264479d
2dust <31833384+2dust@users.noreply.github.com>
2024-04-10 10:50:52 +0800

    Built-in routing rule set upgraded to V2

--- v2rayN/v2rayN/Handler/ConfigHandler.cs
+++ v2rayN/v2rayN/Handler/ConfigHandler.cs
@@ -1674,14 +1674,15 @@ namespace v2rayN.Handler
 
         public static int InitBuiltinRouting(Config config, bool blImportAdvancedRules = false)
         {
+            var ver = "V2-";
             var items = LazyConfig.Instance.RoutingItems();
-            if (blImportAdvancedRules || items.Count <= 0)
+            if (blImportAdvancedRules || items.Where(t => t.remarks.StartsWith(ver)).ToList().Count <= 0)
             {
                 var maxSort = items.Count;
                 //Bypass the mainland
                 var item2 = new RoutingItem()
                 {
-                    remarks = "绕过大陆(Whitelist)",
+                    remarks = $"{ver}绕过大陆(Whitelist)",
                     url = string.Empty,
                     sort = maxSort + 1,
                 };
@@ -1690,7 +1691,7 @@ namespace v2rayN.Handler
                 //Blacklist
                 var item3 = new RoutingItem()
                 {
-                    remarks = "黑名单(Blacklist)",
+                    remarks = $"{ver}黑名单(Blacklist)",
                     url = string.Empty,
                     sort = maxSort + 2,
                 };
@@ -1699,7 +1700,7 @@ namespace v2rayN.Handler
                 //Global
                 var item1 = new RoutingItem()
                 {
-                    remarks = "全局(Global)",
+                    remarks = $"{ver}全局(Global)",
                     url = string.Empty,
                     sort = maxSort + 3,
                 };
