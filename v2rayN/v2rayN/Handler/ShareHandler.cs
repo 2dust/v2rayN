5fe468fa1b1f4507c25161223cdca1b206dcde9a
sincere liu <sincereliulxs@gmail.com>
2024-05-05 22:07:51 +0800

    Optimize code for better performance

--- v2rayN/v2rayN/Handler/ShareHandler.cs
+++ v2rayN/v2rayN/Handler/ShareHandler.cs
@@ -266,19 +266,10 @@ namespace v2rayN.Handler
         {
             if (Utils.IsIpv6(address))
             {
-                if (address.StartsWith('[') && address.EndsWith(']'))
-                {
-                    return address;
-                }
-                else
-                {
-                    return $"[{address}]";
-                }
+                // 检查地址是否已经被方括号包围，如果没有，则添加方括号
+                return address.StartsWith('[') && address.EndsWith(']') ? address : $"[{address}]";
             }
-            else
-            {
-                return address;
-            }
+            return address;  // 如果不是IPv6地址，直接返回原地址
         }
 
         private static int GetStdTransport(ProfileItem item, string? securityDef, ref Dictionary<string, string> dicQuery)
