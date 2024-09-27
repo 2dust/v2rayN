8e02394ff45f60ea0bc67bfd6352cc6e78f77d27
2dust <31833384+2dust@users.noreply.github.com>
2024-09-27 09:58:09 +0800

    Adjustments to automatically update GEO files

--- v2rayN/ServiceLib/Handler/TaskHandler.cs
+++ v2rayN/ServiceLib/Handler/TaskHandler.cs
@@ -12,7 +12,7 @@
         public void RegUpdateTask(Config config, Action<bool, string> update)
         {
             Task.Run(() => UpdateTaskRunSubscription(config, update));
-            //Task.Run(() => UpdateTaskRunGeo(config, update));
+            Task.Run(() => UpdateTaskRunGeo(config, update));
         }
 
         private async Task UpdateTaskRunSubscription(Config config, Action<bool, string> update)
@@ -50,12 +50,14 @@
         {
             var autoUpdateGeoTime = DateTime.Now;
 
-            await Task.Delay(1000 * 120);
+            //await Task.Delay(1000 * 120);
             Logging.SaveLog("UpdateTaskRunGeo");
 
             var updateHandle = new UpdateHandler();
             while (true)
             {
+                await Task.Delay(1000 * 3600);
+
                 var dtNow = DateTime.Now;
                 if (config.guiItem.autoUpdateInterval > 0)
                 {
@@ -68,8 +70,6 @@
                         autoUpdateGeoTime = dtNow;
                     }
                 }
-
-                await Task.Delay(1000 * 3600);
             }
         }
     }
