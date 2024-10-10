4f5362fdc445db819aefb55499fe7539a542be54
2dust <31833384+2dust@users.noreply.github.com>
2024-10-10 14:05:14 +0800

    Update build.ps1

--- v2rayN/build.ps1
+++ v2rayN/build.ps1
@@ -10,26 +10,29 @@ Write-Host 'Building'
 dotnet publish `
 	.\v2rayN\v2rayN.csproj `
 	-c Release `
+	-r win-x64 `
 	--self-contained false `
 	-p:PublishReadyToRun=false `
 	-p:PublishSingleFile=true `
-	-o $OutputPath
+	-o "$OutputPath\win-x64"
 
 dotnet publish `
-	.\AmazTool\AmazTool.csproj `
+	.\v2rayN.Desktop\v2rayN.Desktop.csproj `
 	-c Release `
-	--self-contained false `
+	-r linux-x64 `
+	--self-contained true `
 	-p:PublishReadyToRun=false `
 	-p:PublishSingleFile=true `
-	-o $OutputPath
+	-o "$OutputPath\linux-x64"
+
 
 if ( -Not $? ) {
 	exit $lastExitCode
 	}
 
 if ( Test-Path -Path .\bin\v2rayN ) {
-    rm -Force "$OutputPath\*.pdb"
-    rm -Force "$OutputPath\*.xml"
+    rm -Force "$OutputPath\win-x64\*.pdb"
+    rm -Force "$OutputPath\linux-x64\*.pdb"
 }
 
 Write-Host 'Build done'
