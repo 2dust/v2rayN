c95b87624edc84729c2c7c2a2cc9bb52641a3e0b
Slnanx <SudakaLemonTea@outlook.com>
2024-11-17 09:46:58 +0000

    移除自述

--- v2rayN/AmazTool/UpgradeApp.cs
+++ v2rayN/AmazTool/UpgradeApp.cs
@@ -1,33 +1,4 @@
-﻿/**
- * 该程序使用JSON文件对C#应用程序进行本地化。
- * 程序根据系统当前的语言加载相应的语言文件。
- * 如果当前语言不被支持，则默认使用英语。
- * 
- * 库:
- *  - System.Collections.Generic
- *  - System.Globalization
- *  - System.IO
- *  - System.Text.Json
- * 
- * 用法:
- *  - 为每种支持的语言创建JSON文件（例如，en-US.json，zh-CN.json）。
- *  - 将JSON文件放置程序同目录中。
- *  - 运行程序，它将根据系统当前的语言加载翻译。
- *  - 调用方式： localization.Translate("Try_Terminate_Process") //返回一个 string 字符串
- * 示例JSON文件（en.json）：
- * {
- *     "Restart_v2rayN": "Start v2rayN, please wait...",
- *     "Guidelines": "Please run it from the main application."
- * }
- * 
- * 示例JSON文件（zh.json）：
- * {
- *     "Restart_v2rayN": "正在重启，请等待...",
- *     "Guidelines": "请从主应用运行！"
- * }
- */
-
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
