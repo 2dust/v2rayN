3b19bc1a76ea9c14137a52aa87c60922e6078bb6
Slnanx <SudakaLemonTea@outlook.com>
2024-11-17 17:15:56 +0800

    fix

--- v2rayN/AmazTool/Program.cs
+++ v2rayN/AmazTool/Program.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/**
+ * 该程序使用JSON文件对C#应用程序进行本地化。
+ * 程序根据系统当前的语言加载相应的语言文件。
+ * 如果当前语言不被支持，则默认使用英语。
+ * 
+ * 库:
+ *  - System.Collections.Generic
+ *  - System.Globalization
+ *  - System.IO
+ *  - System.Text.Json
+ * 
+ * 用法:
+ *  - 为每种支持的语言创建JSON文件（例如，en.json，zh.json）。
+ *  - 将JSON文件放置程序同目录中。
+ *  - 运行程序，它将根据系统当前的语言加载翻译。
+ *  - 调用方式： localization.Translate("Try_Terminate_Process") //返回一个 string 字符串
+ * 示例JSON文件（en.json）：
+ * {
+ *     "Restart_v2rayN": "Start v2rayN, please wait...",
+ *     "Guidelines": "Please run it from the main application."
+ * }
+ * 
+ * 示例JSON文件（zh.json）：
+ * {
+ *     "Restart_v2rayN": "正在重启，请等待...",
+ *     "Guidelines": "请从主应用运行！"
+ * }
+ */
+
+using System;
 using System.Threading;
 
 namespace AmazTool
@@ -11,14 +40,19 @@ namespace AmazTool
         [STAThread]
         private static void Main(string[] args)
         {
+            var localization = new Localization();
+
             if (args.Length == 0)
             {
-                Console.WriteLine(LocalizationHelper.GetLocalizedValue("Guidelines"));
+                // 不能直接打开更新程序
+                Console.WriteLine(localization.Translate("Guidelines"));
                 Thread.Sleep(5000);
                 return;
             }
 
+            // 解析并拼接命令行参数以获取文件名
             var fileName = Uri.UnescapeDataString(string.Join(" ", args));
+            // 调用升级方法进行文件处理
             UpgradeApp.Upgrade(fileName);
         }
     }
