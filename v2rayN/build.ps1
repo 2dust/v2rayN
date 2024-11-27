a55c65374d25562dfbc11d5afbd00198ed529c1c
2dust <31833384+2dust@users.noreply.github.com>
2024-11-27 14:22:42 +0800

    Update build.ps1

--- v2rayN/build.ps1
+++ v2rayN/build.ps1
@@ -2,57 +2,57 @@ param (
 	[Parameter()]
 	[ValidateNotNullOrEmpty()]
 	[string]
-	$OutputPath = '.\bin\v2rayN'
+	$OutputPath = './bin/v2rayN'
 )
 
 Write-Host 'Building'
 
 dotnet publish `
-	.\v2rayN\v2rayN.csproj `
+	./v2rayN/v2rayN.csproj `
 	-c Release `
 	-r win-x64 `
 	--self-contained false `
 	-p:PublishReadyToRun=false `
 	-p:PublishSingleFile=true `
-	-o "$OutputPath\win-x64"
+	-o "$OutputPath/win-x64"
 
 dotnet publish `
-	.\v2rayN\v2rayN.csproj `
+	./v2rayN/v2rayN.csproj `
 	-c Release `
 	-r win-arm64 `
 	--self-contained false `
 	-p:PublishReadyToRun=false `
 	-p:PublishSingleFile=true `
-	-o "$OutputPath\win-arm64"
+	-o "$OutputPath/win-arm64"
 
 dotnet publish `
-	.\v2rayN.Desktop\v2rayN.Desktop.csproj `
+	./v2rayN.Desktop/v2rayN.Desktop.csproj `
 	-c Release `
 	-r linux-x64 `
 	--self-contained true `
 	-p:PublishReadyToRun=false `
 	-p:PublishSingleFile=true `
-	-o "$OutputPath\linux-x64"
+	-o "$OutputPath/linux-x64"
 	
 dotnet publish `
-	.\v2rayN.Desktop\v2rayN.Desktop.csproj `
+	./v2rayN.Desktop/v2rayN.Desktop.csproj `
 	-c Release `
 	-r linux-arm64 `
 	--self-contained true `
 	-p:PublishReadyToRun=false `
 	-p:PublishSingleFile=true `
-	-o "$OutputPath\linux-arm64"
+	-o "$OutputPath/linux-arm64"
  
 
 if ( -Not $? ) {
 	exit $lastExitCode
 	}
 
-if ( Test-Path -Path .\bin\v2rayN ) {
-    rm -Force "$OutputPath\win-x64\*.pdb"
-    rm -Force "$OutputPath\win-arm64\*.pdb"
-    rm -Force "$OutputPath\linux-x64\*.pdb"
-    rm -Force "$OutputPath\linux-arm64\*.pdb"
+if ( Test-Path -Path ./bin/v2rayN ) {
+    rm -Force "$OutputPath/win-x64/*.pdb"
+    rm -Force "$OutputPath/win-arm64/*.pdb"
+    rm -Force "$OutputPath/linux-x64/*.pdb"
+    rm -Force "$OutputPath/linux-arm64/*.pdb"
 }
 
 Write-Host 'Build done'
