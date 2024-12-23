c78b733850dfe57580da12671312e2229cbab07e
2dust <31833384+2dust@users.noreply.github.com>
2024-12-23 13:59:55 +0800

    Revert "update build macos"
    This reverts commit a57c83125e152f66421a9c71a97996812a1e650b.

--- v2rayN/build-osx.sh
+++ v2rayN/build-osx.sh
@@ -2,17 +2,16 @@
 
 echo 'Building'
 
-OutputPath='./bin/v2rayN/osx-x64'
-OutputPathArm64='./bin/v2rayN/osx-arm64'
+OutputPath='./bin/v2rayN'
 
-dotnet publish 	./v2rayN.Desktop/v2rayN.Desktop.csproj 	-c Release 	-r osx-x64 	--self-contained true 	-p:PublishReadyToRun=false 	-p:PublishSingleFile=true 	-o $OutputPath
-dotnet publish 	./v2rayN.Desktop/v2rayN.Desktop.csproj 	-c Release 	-r osx-arm64 	--self-contained true 	-p:PublishReadyToRun=false 	-p:PublishSingleFile=true 	-o $OutputPathArm64
+dotnet publish 	./v2rayN.Desktop/v2rayN.Desktop.csproj 	-c Release 	-r osx-x64 	--self-contained true 	-p:PublishReadyToRun=false 	-p:PublishSingleFile=true 	-o "${OutputPath}/osx-x64"
+dotnet publish 	./v2rayN.Desktop/v2rayN.Desktop.csproj 	-c Release 	-r osx-arm64 	--self-contained true 	-p:PublishReadyToRun=false 	-p:PublishSingleFile=true 	-o "${OutputPath}/osx-arm64"
 
-rm -rf "$OutputPath/*.pdb"
-rm -rf "$OutputPathArm64/*.pdb"
+rm -rf "$OutputPath/osx-x64/*.pdb"
+rm -rf "$OutputPath/osx-arm64/*.pdb"
 
 echo 'Build done'
 
-7z a  v2rayN-macos-64.zip $OutputPath
-7z a  v2rayN-macos-arm64.zip $OutputPathArm64
+ls $OutputPath
+7z a  v2rayN-osx.zip $OutputPath
 exit 0
