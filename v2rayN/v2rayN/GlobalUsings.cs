9bbb4f2bab76931be0aae69933dcf870c1db7aec
Juan Vargas <66626747+v4rgas@users.noreply.github.com>
2025-03-06 20:15:14 -0300

    Update GlobalUsings.cs

--- v2rayN/v2rayN/GlobalUsings.cs
+++ v2rayN/v2rayN/GlobalUsings.cs
@@ -2,7 +2,7 @@
 global using ServiceLib.Base;
 global using ServiceLib.Common;
 global using ServiceLib.Enums;
-global using ServiceLib.Handler;
+
 global using ServiceLib.Models;
 global using ServiceLib.Resx;
 global using ServiceLib.ViewModels;
