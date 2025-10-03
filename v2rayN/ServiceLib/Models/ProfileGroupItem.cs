33f5d20022cdcecd27115f80f14a8478c5e93556
2dust <31833384+2dust@users.noreply.github.com>
2025-10-03 17:11:56 +0800

    Update ProfileGroupItem.cs

--- v2rayN/ServiceLib/Models/ProfileGroupItem.cs
+++ v2rayN/ServiceLib/Models/ProfileGroupItem.cs
@@ -1,4 +1,5 @@
 using SQLite;
+
 namespace ServiceLib.Models;
 
 [Serializable]
@@ -10,4 +11,48 @@ public class ProfileGroupItem
     public string ChildItems { get; set; }
 
     public EMultipleLoad MultipleLoad { get; set; } = EMultipleLoad.LeastPing;
+
+    public bool HasCycle()
+    {
+        return HasCycle(new HashSet<string>(), new HashSet<string>());
+    }
+
+    public bool HasCycle(HashSet<string> visited, HashSet<string> stack)
+    {
+        if (string.IsNullOrEmpty(ParentIndexId))
+            return false;
+
+        if (stack.Contains(ParentIndexId))
+            return true;
+
+        if (visited.Contains(ParentIndexId))
+            return false;
+
+        visited.Add(ParentIndexId);
+        stack.Add(ParentIndexId);
+
+        if (string.IsNullOrEmpty(ChildItems))
+        {
+            return false;
+        }
+
+        var childIds = Utils.String2List(ChildItems)
+            .Where(p => !string.IsNullOrEmpty(p))
+            .ToList();
+
+        var childProfiles = childIds.Select(ProfileGroupItemManager.Instance.GetOrDefault)//这里是内存访问
+            .Where(p => p != null)
+            .ToList();
+
+        foreach (var child in childProfiles)
+        {
+            if (child.HasCycle(visited, stack))
+            {
+                return true;
+            }
+        }
+
+        stack.Remove(ParentIndexId);
+        return false;
+    }
 }
--- v2rayN/ServiceLib/Services/CoreConfig/V2ray/V2rayOutboundService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/V2ray/V2rayOutboundService.cs
@@ -496,7 +496,8 @@ public partial class CoreConfigV2rayService
                 return -1;
             }
 
-            var hasCycle = await node.HasCycle(new HashSet<string>(), new HashSet<string>());
+            var hasCycle = profileGroupItem.HasCycle();
+            //var hasCycle = await node.HasCycle(new HashSet<string>(), new HashSet<string>());
             if (hasCycle)
             {
                 return -1;
