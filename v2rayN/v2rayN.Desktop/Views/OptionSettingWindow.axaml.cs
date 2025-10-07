e2ead5605489393520ab5450c84372fe122cbc5e
2dust <31833384+2dust@users.noreply.github.com>
2025-10-07 10:02:15 +0800

    Revert "Disable AutoHideStartup for Linux"
    This reverts commit 09f27e345575f9044c035a7fea055a83f5136fa2.

--- v2rayN/v2rayN.Desktop/Views/OptionSettingWindow.axaml.cs
+++ v2rayN/v2rayN.Desktop/Views/OptionSettingWindow.axaml.cs
@@ -136,8 +136,6 @@ public partial class OptionSettingWindow : WindowBase<OptionSettingViewModel>
         {
             txbSettingsExceptionTip.IsVisible = false;
             panSystemProxyAdvanced.IsVisible = false;
-            labAutoHideStartup.IsVisible = false;
-            togAutoHideStartup.IsVisible = false;
         }
         else if (Utils.IsOSX())
         {
