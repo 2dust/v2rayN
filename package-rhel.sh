fd8732ecc3de9a3b8475fbe3bad3387b6a79a430
JieXu <xujie0739@gmail.com>
2025-10-23 10:45:12 +0800

    Update package-rhel.sh

--- package-rhel.sh
+++ package-rhel.sh
@@ -617,7 +617,6 @@ Source0:        __PKGROOT__.tar.gz
 Requires:       cairo, pango
 Requires:       glibc >= 2.34
 Requires:       fontconfig >= 2.14
-Requires:       openssl >= 1:3.0
 Requires:       desktop-file-utils >= 0.26
 Requires:       xdg-utils >= 1.1.3
 
