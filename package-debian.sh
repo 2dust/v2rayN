8501c42f4f3a1cb1bef4012259082c5f4588b002
JieXu <xujie0739@gmail.com>
2025-10-23 12:22:50 +0800

    Update package-debian.sh

--- package-debian.sh
+++ package-debian.sh
@@ -28,7 +28,7 @@ Package: v2rayN
 Version: $Version
 Architecture: $Arch2
 Maintainer: https://github.com/2dust/v2rayN
-Depends: libc6 (>= 2.34), fontconfig (>= 2.14), desktop-file-utils (>= 0.26), coreutils (>= 8.32), xdg-utils (>= 1.1.3), libx11-6, openssl
+Depends: libc6 (>= 2.34), fontconfig (>= 2.14), desktop-file-utils (>= 0.26), xdg-utils (>= 1.1.3), coreutils (>= 8.32), libx11-6, openssl
 Description: A GUI client for Windows and Linux, support Xray core and sing-box-core and others
 EOF
 
