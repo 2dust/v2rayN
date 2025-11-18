f57cc410587d686e882d60b6189a5f5662cdd27b
JieXu <xujie0739@gmail.com>
2025-11-18 22:06:28 +0800

    Update AppBuilderExtension.cs

--- v2rayN/v2rayN.Desktop/Common/AppBuilderExtension.cs
+++ v2rayN/v2rayN.Desktop/Common/AppBuilderExtension.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using Avalonia;
 using Avalonia.Media;
@@ -8,22 +9,22 @@ public static class AppBuilderExtension
 {
     public static AppBuilder WithFontByDefault(this AppBuilder appBuilder)
     {
+        var fallbacks = new List<FontFallback>();
+
         var notoSansSc = new FontFamily(Path.Combine(Global.AvaAssets, "Fonts#Noto Sans SC"));
+        fallbacks.Add(new FontFallback { FontFamily = notoSansSc });
 
-        var fallbacks = new[]
+        if (OperatingSystem.IsLinux())
         {
-            new FontFallback { FontFamily = notoSansSc },
-
-            OperatingSystem.IsLinux()
-                ? new FontFallback { FontFamily = new FontFamily("Noto Color Emoji") }
-                : null
-        };
-
-        var validFallbacks = fallbacks.Where(f => f is not null).ToArray()!;
+            fallbacks.Add(new FontFallback
+            {
+                FontFamily = new FontFamily("Noto Color Emoji")
+            });
+        }
 
         return appBuilder.With(new FontManagerOptions
         {
-            FontFallbacks = validFallbacks
+            FontFallbacks = fallbacks.ToArray()
         });
     }
 }
