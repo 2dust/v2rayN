054efeb32c3bbd9f5b9c6c0be0a836fd1f7163dc
JieXu <xujie0739@gmail.com>
2025-11-19 16:47:00 +0800

    [PR]改进Emoji字体兼容性 (#8346)
    * Update AppBuilderExtension.cs
    
    * Update package-rhel.sh
    
    * Update package-debian.sh
    
    * Update AppBuilderExtension.cs
    
    * Update AppBuilderExtension.cs
    
    * Update AppBuilderExtension.cs
    
    * Withdraw
    
    * Update AppBuilderExtension.cs

--- v2rayN/v2rayN.Desktop/Common/AppBuilderExtension.cs
+++ v2rayN/v2rayN.Desktop/Common/AppBuilderExtension.cs
@@ -1,14 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Avalonia;
+using Avalonia.Media;
+
 namespace v2rayN.Desktop.Common;
 
 public static class AppBuilderExtension
 {
     public static AppBuilder WithFontByDefault(this AppBuilder appBuilder)
     {
-        var uri = Path.Combine(Global.AvaAssets, "Fonts#Noto Sans SC");
-        return appBuilder.With(new FontManagerOptions()
+        var fallbacks = new List<FontFallback>();
+        
+        var notoSansSc = new FontFamily(Path.Combine(Global.AvaAssets, "Fonts#Noto Sans SC"));
+        fallbacks.Add(new FontFallback { FontFamily = notoSansSc });
+
+        if (OperatingSystem.IsLinux())
         {
-            //DefaultFamilyName = uri,
-            FontFallbacks = new[] { new FontFallback { FontFamily = new FontFamily(uri) } }
+            fallbacks.Add(new FontFallback
+            {
+                FontFamily = new FontFamily("Noto Color Emoji")
+            });
+        }
+
+        return appBuilder.With(new FontManagerOptions
+        {
+            FontFallbacks = fallbacks.ToArray()
         });
     }
 }
