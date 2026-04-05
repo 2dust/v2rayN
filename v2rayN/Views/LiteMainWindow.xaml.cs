9f26386283c43266baa8c5e2ab4265dfc2049c36
antallusion <enosial@ya.ru>
2026-04-05 03:23:21 +0300

    Create LiteMainWindow.xaml.cs code-behind file

---
+++ v2rayN/Views/LiteMainWindow.xaml.cs
@@ -0,0 +1,16 @@
+// This is the code-behind for the LiteMainWindow.xaml
+
+using System.Windows;
+
+namespace v2rayN.Views
+{
+    public partial class LiteMainWindow : Window
+    {
+        public LiteMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Add your code here
+    }
+}
