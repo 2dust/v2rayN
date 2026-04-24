738aa785ee76cb87018d79282935b0382b67df8e
JieXu <xujie0739@gmail.com>
2026-04-24 17:14:21 +0800

    Update package-debian-riscv.sh

--- package-debian-riscv.sh
+++ package-debian-riscv.sh
@@ -17,7 +17,7 @@ DOTNET_RISCV_VERSION="10.0.107"
 DOTNET_RISCV_BASE="https://github.com/xujiegb/dotnet-riscv/releases/download"
 DOTNET_RISCV_FILE="dotnet-sdk-${DOTNET_RISCV_VERSION}-linux-riscv64.tar.gz"
 DOTNET_SDK_URL="${DOTNET_RISCV_BASE}/${DOTNET_RISCV_VERSION}/${DOTNET_RISCV_FILE}"
-SKIA_VER="${SKIA_VER:-3.119.3-preview.1.1}"
+SKIA_VER="${SKIA_VER:-3.119.2}"
 HARFBUZZ_VER="${HARFBUZZ_VER:-8.3.1.3}"
 
 OS_ID=""
