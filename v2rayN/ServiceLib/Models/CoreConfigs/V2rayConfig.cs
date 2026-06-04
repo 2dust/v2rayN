0c83a36fff4aa30e0457c562359f24eea9e5ee4b
DHR60 <dehongren60@gmail.com>
2026-06-04 22:32:22 +0800

    Set xray default tun dns

--- v2rayN/ServiceLib/Models/CoreConfigs/V2rayConfig.cs
+++ v2rayN/ServiceLib/Models/CoreConfigs/V2rayConfig.cs
@@ -86,7 +86,7 @@ public class Inboundsettings4Ray
 
     public string? autoOutboundsInterface { get; set; }
 
-    // public List<string>? dns { get; set; }
+    public List<string>? dns { get; set; }
 }
 
 public class UsersItem4Ray
