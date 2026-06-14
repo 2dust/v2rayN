4e1331c353c7d67aa08f5d35772676fddce798eb
JieXu <xujie0739@gmail.com>
2026-06-14 09:34:51 +0800

    Update package-rhel-riscv.sh

--- package-rhel-riscv.sh
+++ package-rhel-riscv.sh
@@ -501,7 +501,7 @@ write_spec_file() {
 Name:           v2rayN
 Version:        __VERSION__
 Release:        1%{?dist}
-Summary:        v2rayN (Avalonia) GUI client for Linux (riscv64)
+Summary:        v2rayN (Avalonia) GUI client for Linux
 License:        GPL-3.0-only
 URL:            https://github.com/2dust/v2rayN
 BugURL:         https://github.com/2dust/v2rayN/issues
