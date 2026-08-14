b3df42b9ba117d180e258cc67b37283347dc8290
DHR60 <192860629+DHR60@users.noreply.github.com>
2026-08-14 02:19:56 +0000

    Sync finalmask order (#9943)

--- v2rayN/ServiceLib/Services/CoreConfig/V2ray/V2rayOutboundService.cs
+++ v2rayN/ServiceLib/Services/CoreConfig/V2ray/V2rayOutboundService.cs
@@ -512,6 +512,7 @@ public partial class CoreConfigV2rayService
                             settings = new MaskSettings4Ray { value = kcpSeed },
                         });
                     }
+                    kcpFinalmask.udp?.Reverse();
                     streamSettings.kcpSettings = kcpSettings;
                     streamSettings.finalmask = kcpFinalmask;
                     break;
@@ -666,6 +667,7 @@ public partial class CoreConfigV2rayService
                         version = 2,
                         auth = _node.Password,
                     };
+                    hy2Finalmask.udp?.Reverse();
                     streamSettings.finalmask = hy2Finalmask;
                     break;
 
